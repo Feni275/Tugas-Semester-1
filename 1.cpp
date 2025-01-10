11bcb7eea45175b56a1f368e9714fd7c0876bae4
Feni275 <fenimustika275@gmail.com>
2025-01-10 13:52:42 +0700

    Modul 6 Tugas Mandiri

--- 1.cpp
+++ 1.cpp
@@ -1,26 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	int jam_masuk, jam_keluar, lama_bekerja;
-	
-	printf("Masukkan jam masuk (1-12): ");
-	scanf("%d", &jam_masuk);
-	
-	printf("Masukkan jam keluar (1-12): ");
-	scanf("%d", &jam_keluar);
-	
-	if (jam_masuk < 1 || jam_masuk > 12 || jam_keluar < 1 || jam_keluar > 12) {
-		printf("Jam masuk dan keluar harus dalam rentang 1-12.\n");
-		return 1;
+	for (int i = 1; i <= 10; i++) {
+		printf("%d", i * 10);
+		if (i < 10) {
+			printf(" ");
+		}
 	}
-	
-	if (jam_keluar >= jam_masuk) {
-		lama_bekerja = jam_keluar - jam_masuk;
-	} else {
-		lama_bekerja = (12 - jam_masuk) + jam_keluar;
-	}
-	
-	printf("Lama bekerja %d jam\n", lama_bekerja);
-	
+	printf("\n");
 	return 0;
 }
--- 2.cpp
+++ 2.cpp
@@ -1,33 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int kode;
-    char jenis;
-    float harga, harga_setelah_diskon, diskon;
-
-    printf("Masukkan kode: ");
-    scanf("%d", &kode);
-    
-    printf("Masukkan jenis (A/B/C): ");
-    scanf(" %c", &jenis);
-    
-    printf("Masukkan harga: ");
-    scanf("%f", &harga);
-
-    if (jenis == 'A' || jenis == 'a') {
-        diskon = 0.10; // 10%
-    } else if (jenis == 'B' || jenis == 'b') {
-        diskon = 0.15; // 15%
-    } else if (jenis == 'C' || jenis == 'c') {
-        diskon = 0.20; // 20%
-    } else {
-        printf("Jenis barang tidak valid!\n");
-        return 1;
-    }
-
-    harga_setelah_diskon = harga - (harga * diskon);
-
-    printf("Jenis barang %c mendapat diskon = %.0f%%, Harga setelah didiskon = %.2f\n", jenis, diskon * 100, harga_setelah_diskon);
-
-    return 0;
+	for (int i = 100; i >= 55; i -= 5) {
+		printf("%d", i);
+		if (i > 55) {
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return 0;
 }
